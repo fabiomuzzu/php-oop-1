b2a78fbc9bc6066e23967c836160b302cfc20992
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-30 14:45:35 +0100

    completed contruct function

--- index.php
+++ index.php
@@ -8,20 +8,24 @@
         public $director;
         public $production;
 
-        function __construct($_director, $_production){
+        // Creazione della funzione construct a cui passerò i parametri di director e production
+        function __construct($_director, $_production)
+        {
             $this->director = $_director;
             $this->production = $_production;
         }
     }
 
     // Creo una istanza della classe movie
-    $movie_1 = new Movie();
+    $movie_1 = new Movie("Chris Columbus", "David Heyman");
 
     // Assegno dei valori agli attributi dell'istanza movie
     $movie_1->title = "Harry Potter e la pietra filosofale";
     $movie_1->genre = "Fantasy";
     $movie_1->year = 2001;
     $movie_1->rate = 5;
+    
+    var_dump($movie_1);
 ?>
 
 <!DOCTYPE html>
